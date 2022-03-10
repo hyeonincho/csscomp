d4309736d26d98a02b4f7a9374ee742906daa25c
Cho HyeonIn <kasitw1124@gmail.com>
2022-03-10 19:27:04 +0900

    2주차

--- parse.js
+++ parse.js
@@ -9,29 +9,25 @@ const supportScore = {
     n: 6,
 }
 const REGEXP_START_WITH_CSS = /^(css)/i;
+const REGEXP_START_WITH_CSS_HYPHEN = /^(css[0-9]?)-/;
 const REGEXP_CSS_AND_HYPHEN = /(css)|-/g;
+const REGEXP_NO_ALPHABET_HYPHEN = /[^a-z- ]/g;
 const REGEXP_HYPHEN = /-/g;
 const REGEXP_INSIDE_BRACKET = /{([^}]+)}/g;
 
-const removeRegexpString = (regexp, string) => {
-
-    if (string.startsWith('css')) {
-        string = string.substring(3)
-    }
-
-    string = string.replace(regexp, '');
+const RegexpToSpace = (regexp, string) => {
+    string = string.replace(regexp, ' ');
     return string;
 }
 
 const loadJson = async () => {
     let response = await fetch("./data-2.0.json")
     let jsonData = await response.json();
-    console.log(jsonData)
     return jsonData.data;
 }
 
-const getCssMap = (canIUseObj) => {
-    let cssMap = new Map();
+const getCssArray = (canIUseObj) => {
+    let cssArray = [];
 
     for (let prop in canIUseObj) {
         if (!Object.prototype.hasOwnProperty.call(canIUseObj, prop)) {
@@ -41,12 +37,19 @@ const getCssMap = (canIUseObj) => {
         const category = canIUseObj[prop]['categories'][0]
         // TODO: value/att 분리
         if (REGEXP_START_WITH_CSS.test(category)) {
-            let mapKey = removeRegexpString(REGEXP_CSS_AND_HYPHEN, prop);
-            cssMap.set(mapKey, canIUseObj[prop].stats);
+            let keyString = `${prop} ${canIUseObj[prop].title} ${canIUseObj[prop].keywords} ${canIUseObj[prop].description}`.toLowerCase();
+            keyString = RegexpToSpace(REGEXP_START_WITH_CSS_HYPHEN, keyString);
+            keyString = RegexpToSpace(REGEXP_NO_ALPHABET_HYPHEN, keyString);
+            let cssObj = {
+                key: keyString,
+                support: canIUseObj[prop].stats,
+                info: canIUseObj[prop],
+            }
+            cssArray.push(cssObj)
         }
     }
 
-    return cssMap;
+    return cssArray;
 }
 
 let proto = Element.prototype;
@@ -74,7 +77,7 @@ let getUserCssPropertySet = (styleSheetString, useCssPropertySet) => {
 
     styleArray.forEach(styleString => {
         let style = styleString.split(':');
-        useCssPropertySet.add(removeRegexpString(REGEXP_HYPHEN, style[0].substring(1)));
+        useCssPropertySet.add(style[0].substring(1));
         // TODO: style[1] 중 특별한 value 골라서 value Set에 넣던가 해야됨
     });
     return useCssPropertySet;
@@ -84,7 +87,6 @@ let userCssPropertySet = new Set();
 for (let i = 0; i < cssRules.length; ++i) {
     getUserCssPropertySet(cssRules[i].cssText, userCssPropertySet)
 }
-console.log(userCssPropertySet)
 
 // let getAppliedCss = function (elm) {
 //     // get only the css rules that matches that element
@@ -124,10 +126,69 @@ const getSupportStats = (userStyleSet, canIUseMap) => {
     return supportStats;
 }
 
+const exactWordRegex = (string) => new RegExp("(^|\\s)" + string + "(\\s|$)");
+const getSupportStatFromArray = (userStyleSet, canIUseArr) => {
+    let supportStats = [];
+    let searchFailedStyles = new Set(userStyleSet);
+
+    let exactWordRegexMap = new Map();
+    for (style of userStyleSet) {
+        exactWordRegexMap.set(style, exactWordRegex(style));
+    }
+
+    for (let i = 0; i < canIUseArr.length; ++i) {
+        let keyword = canIUseArr[i].key;
+        let support = canIUseArr[i].support;
+        for (style of userStyleSet) {
+            if (exactWordRegexMap.get(style).test(keyword)) {
+                searchFailedStyles.delete(style);
+                supportStats.push({
+                    support: support,
+                    style: style,
+                    keyword: keyword,
+                })
+            }
+        }
+    }
+    for (failed of searchFailedStyles) {
+        console.log(`fail: ${failed}`)
+    }
+    return supportStats;
+}
+
 loadJson().then(response => {
-    let cssMap = getCssMap(response);
-    console.log(cssMap);
-    let supportStats = getSupportStats(userCssPropertySet, cssMap);
-    console.log(supportStats)
+    let cssArray = getCssArray(response);
+    console.log(cssArray)
+    const supportResult = getSupportStatFromArray(userCssPropertySet, cssArray);
+    console.log(supportResult)
 });
 
+function getStyle(el, styleProp) {
+    var value, defaultView = (el.ownerDocument || document).defaultView;
+    // W3C standard way:
+    if (defaultView && defaultView.getComputedStyle) {
+        // sanitize property name to css notation
+        // (hypen separated words eg. font-Size)
+        styleProp = styleProp.replace(/([A-Z])/g, "-$1").toLowerCase();
+        return defaultView.getComputedStyle(el, null).getPropertyValue(styleProp);
+    } else if (el.currentStyle) { // IE
+        // sanitize property name to camelCase
+        styleProp = styleProp.replace(/\-(\w)/g, function (str, letter) {
+            return letter.toUpperCase();
+        });
+        value = el.currentStyle[styleProp];
+        // convert other units to pixels on IE
+        if (/^\d+(em|pt|%|ex)?$/i.test(value)) {
+            return (function (value) {
+                var oldLeft = el.style.left, oldRsLeft = el.runtimeStyle.left;
+                el.runtimeStyle.left = el.currentStyle.left;
+                el.style.left = value || 0;
+                value = el.style.pixelLeft + "px";
+                el.style.left = oldLeft;
+                el.runtimeStyle.left = oldRsLeft;
+                return value;
+            })(value);
+        }
+        return value;
+    }
+}
